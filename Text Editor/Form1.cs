cf38efa673b52fb656f648d269e33880c05d06fb
Zachary-Rude <zacharyrude10@gmail.com>
2023-03-22 20:41:47 -0700

    Fixed bug where unsaved changes dialog would give a blank string when creating a new file

--- Text Editor/Form1.cs
+++ Text Editor/Form1.cs
@@ -25,6 +25,7 @@ namespace Text_Editor
             mainEditor.WordWrap = Properties.Settings.Default.EnableWordWrap;
             mainEditor.Font = Properties.Settings.Default.Font;
             mainEditor.AutoWordSelection = false;
+            path = "Untitled";
         }
 
         public Form1(string fileName) : this()
@@ -72,7 +73,7 @@ namespace Text_Editor
                 }
             }
             this.Text = "Untitled - Notepad.NET";
-            path = string.Empty;
+            path = "Untitled";
             mainEditor.Clear();
             this.Text = this.Text.Replace("*", "");
         }
