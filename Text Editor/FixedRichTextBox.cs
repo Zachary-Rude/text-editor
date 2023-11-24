0591382f1413e0457343d842db4cffd12de8de5b
Zachary-Rude <zacharyrude10@gmail.com>
2023-11-24 10:02:23 -0800

    Add horizontal mouse wheel scrolling

--- Text Editor/FixedRichTextBox.cs
+++ Text Editor/FixedRichTextBox.cs
@@ -140,6 +140,24 @@ namespace Text_Editor
                 base.AutoWordSelection = true;
                 base.AutoWordSelection = false;
             }
+            this.MouseWheel += this.OnMouseWheel;
+        }
+
+        private const int WM_SCROLL = 276; // Horizontal scroll 
+        private const int SB_LINELEFT = 0; // Scrolls one cell left 
+        private const int SB_LINERIGHT = 1; // Scrolls one line right
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        private void OnMouseWheel(object sender, MouseEventArgs e)
+        {
+            if (ModifierKeys == Keys.Shift)
+            {
+                var direction = e.Delta > 0 ? SB_LINELEFT : SB_LINERIGHT;
+
+                SendMessage(this.Handle, WM_SCROLL, (IntPtr)direction, IntPtr.Zero);
+            }
         }
     }
 }
