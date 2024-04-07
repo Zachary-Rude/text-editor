87dbf294257f12a6679d58a52709721c08ef4ba0
Zachary-Rude <zacharyrude10@gmail.com>
2024-04-07 09:33:43 -0700

    Update about box icon and add icon for right-to-left text

--- Text Editor/Properties/Resources.Designer.cs
+++ Text Editor/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace Text_Editor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap baseline_format_textdirection_r_to_l_black_24dp {
+            get {
+                object obj = ResourceManager.GetObject("baseline_format_textdirection_r_to_l_black_24dp", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap baseline_help_black_24dp {
             get {
                 object obj = ResourceManager.GetObject("baseline_help_black_24dp", resourceCulture);
